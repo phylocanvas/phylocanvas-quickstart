a2dcd6b15debfc518d98bf3cd8ce945141fc251f
Richard Goater <richard.goater27@gmail.com>
2015-08-05 18:00:10 +0100

    Create working example with single plugin

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import PhyloCanvas from 'PhyloCanvas';
+
+import historyPlugin from 'phylocanvas-plugin-history';
+
+PhyloCanvas.plugin(historyPlugin);
+
+export default PhyloCanvas;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/* eslint es6: false */
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    library: 'PhyloCanvas',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /webpack/, loader: 'babel?stage=0' },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
+};
