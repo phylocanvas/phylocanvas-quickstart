ee46f3c8389f177f1a5d57447d4f9e8fb3b9d1fd
Richard Goater <richard.goater27@gmail.com>
2015-08-08 12:03:25 +0100

    Add metadata plugin, update example

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import PhyloCanvas from 'PhyloCanvas';
 
 import historyPlugin from 'phylocanvas-plugin-history';
 import ajaxPlugin from 'phylocanvas-plugin-ajax';
+import metadataPlugin from 'phylocanvas-plugin-metadata';
 
 PhyloCanvas.plugin(historyPlugin);
 PhyloCanvas.plugin(ajaxPlugin);
+PhyloCanvas.plugin(metadataPlugin);
 
 export default PhyloCanvas;
