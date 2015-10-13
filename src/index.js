60956a740355b593ed371f2c86b5fe9c09cb2248
Richard Goater <richard.goater27@gmail.com>
2015-10-13 20:59:37 +0100

    Add context menu plugin and latest 2.0.0

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import PhyloCanvas from 'PhyloCanvas';
 import historyPlugin from 'phylocanvas-plugin-history';
 import ajaxPlugin from 'phylocanvas-plugin-ajax';
 import metadataPlugin from 'phylocanvas-plugin-metadata';
+import contextMenuPlugin from 'phylocanvas-plugin-context-menu';
 
 PhyloCanvas.plugin(historyPlugin);
 PhyloCanvas.plugin(ajaxPlugin);
 PhyloCanvas.plugin(metadataPlugin);
+PhyloCanvas.plugin(contextMenuPlugin);
 
 export default PhyloCanvas;
